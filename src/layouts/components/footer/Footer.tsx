7deecd41faef1583ccbb61a1ffd12521a066d2e0
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-06-27 16:50:52 +0700

    app: setup translate - i18n

--- src/layouts/components/footer/Footer.tsx
+++ src/layouts/components/footer/Footer.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import cookie from "react-cookies";
 
-import VBC_Logo from "../../../assets/svg/logo_vbc.svg";
 import "./Footer.scss";
+import VBC_Logo from "../../../assets/svg/logo_vbc.svg";
 
 const Footer = () => {
+  const { t, i18n } = useTranslation("common");
+
+  const handleChangeLang = (lang: string) => {
+    i18n.changeLanguage(lang);
+    cookie.save("lang", lang, { path: "/" });
+  };
+
+  useEffect(() => {
+    let lang = cookie.load("lang");
+    if (lang) {
+      i18n.changeLanguage(lang);
+    }
+  }, []);
+
   return (
     <>
       <div className="app-footer">
@@ -30,33 +46,41 @@ const Footer = () => {
             <div className="app-footer--sitemap">
               <p className="app-footer--sitemap--title">Sitemap</p>
               <Link to="/" className="app-footer--sitemap--link">
-                Introduction
+                {t("nav.home")}
               </Link>
               <Link to="/marketplace" className="app-footer--sitemap--link">
-                Marketplace
+                {t("nav.market")}
               </Link>
               <Link to="/reward" className="app-footer--sitemap--link">
-                Rewards
+                {t("nav.rewards")}
               </Link>
               <Link to="/blog" className="app-footer--sitemap--link">
-                Blog
+                {t("nav.blog")}
               </Link>
               <Link to="/about" className="app-footer--sitemap--link">
-                About
+                {t("nav.about")}
               </Link>
             </div>
 
             <div className="app-footer--company">
-              <p className="app-footer--company--title">Company</p>
-              <p>Help & Support</p>
-              <p>Term & Conditions</p>
-              <p>Privacy Policy</p>
+              <p className="app-footer--company--title">
+                {t("footer.company.title")}
+              </p>
+              <p>{t("footer.company.about")}</p>
+              <p>{t("footer.company.help")}</p>
+              <p>{t("footer.company.policy")}</p>
             </div>
 
             <div className="app-footer--language">
-              <p className="app-footer--language--title">Language</p>
-              <p>English</p>
-              <p>Vietnamese</p>
+              <p className="app-footer--language--title">
+                {t("footer.lang.title")}
+              </p>
+              <p onClick={() => handleChangeLang("en")}>
+                {t("footer.lang.en")}
+              </p>
+              <p onClick={() => handleChangeLang("vi")}>
+                {t("footer.lang.vi")}
+              </p>
             </div>
           </div>
         </div>
