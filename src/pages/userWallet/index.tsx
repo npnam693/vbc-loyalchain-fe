0f705fc46dc2f9a7d4963bd894030b8434f0168d
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-06-29 21:38:34 +0700

    edit README.md

--- src/pages/userWallet/index.tsx
+++ src/pages/userWallet/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button } from "antd";
+import { useAppSelector } from "../../state/hooks";
 
 const Wallet = () => {
-  return <div>Wallet</div>;
+  const userState = useAppSelector((state) => state.userState);
+
+  const transferMoney = () => {};
+
+  return (
+    <div>
+      Wallet
+      <p>Address: {userState.address}</p>
+      <p>Balance: {userState.balance}</p>
+      <Button>Transfer</Button>
+    </div>
+  );
 };
 
 export default Wallet;
